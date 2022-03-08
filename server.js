1add8ec5323dde6276c2980610b260561e1910cf
MLuqman150 <muhammadluqman12080@outlook.com>
2022-03-09 02:01:00 +0500

    Commited by Luqman

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express();
 
 const port = process.env.PORT || 2500;
 
+// app.use(express.static(path.join(__dirname,"/public")));
 
+app.get('/', function(req, res){
+    res.sendFile("C:/Users/SAEED COMPUTERS/Desktop/Salahi Karhai/Home.html");
+});
 
 app.listen(port, (req,res) => {
     console.log(`Listening on port ${port}`);
